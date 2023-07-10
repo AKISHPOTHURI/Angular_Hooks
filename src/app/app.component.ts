aa7710cd88e868bbc359c73c6a7caf4b95e4112c
AKISHPOTHURI <akishpothuri@gmail.com>
2023-07-10 14:15:48 +0530

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'experimentangular';
+  title = 'Angular Hooks';
   myObservable: any;
   ngOnInit() {
     this.myObservable = new Observable((observer) => {
